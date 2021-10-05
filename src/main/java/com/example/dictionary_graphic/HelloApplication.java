883b85185d7b39d803f8a284543fbdae488b3c5c
Mint1832 <mint18032@gmail.com>
2021-10-05 17:06:22 +0700

    Add basic graphic.

--- src/main/java/com/example/dictionary_graphic/HelloApplication.java
+++ src/main/java/com/example/dictionary_graphic/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.dictionary_graphic;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Image logo = new Image("/com/example/dictionary_graphic/logo.jpg");
+        stage.getIcons().add(logo);
+        stage.setTitle("Khanh & Minh Dictionary");
         stage.setScene(scene);
         stage.show();
     }
