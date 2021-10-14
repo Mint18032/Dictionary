07c5a7aab588a22a2b3104d8ed17933cfd1721db
Mint1832 <mint18032@gmail.com>
2021-10-14 19:54:38 +0700

    Add effects.

--- src/main/java/com/example/dictionary_graphic/HelloController.java
+++ src/main/java/com/example/dictionary_graphic/HelloController.java
@@ -23,8 +23,6 @@ public class HelloController implements Initializable {
     ArrayList<String> listword = Dictionary.listWordTarget();
 
     @FXML
-    private TextField searchBox;
-    @FXML
     private WebView webView;
     @FXML
     private WebView webView2;
@@ -37,12 +35,19 @@ public class HelloController implements Initializable {
     @FXML
     private Text text2;
     @FXML
+    private TextField searchBox;
+    @FXML
     private Tab translate;
     @FXML
     private String target;
     @FXML
+    private Button speak;
+    @FXML
     private ListView<String> listView;
+    @FXML
+    private Button language;
     private boolean updated;
+    private Tooltip tooltip1;
 
     @FXML
     private void onAddButtonClick() {
@@ -53,6 +58,7 @@ public class HelloController implements Initializable {
     public void search(KeyEvent event) {
         listView.getItems().clear();
         listView.getItems().addAll(DictionaryManagement.dictionaryRelatedWord(searchBox.getText()));
+        listView.setOpacity(100);
     }
 
     @FXML
@@ -64,14 +70,21 @@ public class HelloController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        listView.setOpacity(0);
         listView.getItems().addAll(listword);
+        speak.setOpacity(0);
+        webView.setOpacity(0);
         webView.getEngine().setUserStyleSheetLocation(getClass().getResource("/com/example/dictionary_graphic/style.css").toString());
         updated = true;
+        tooltip1 = new Tooltip("Dịch tiếng Anh.");
+        language.setTooltip(tooltip1);
     }
 
     @FXML
     public void onSearchButtonClick(MouseEvent mouseEvent) {
         target = searchBox.getText().trim();
+        speak.setOpacity(100);
+        webView.setOpacity(100);
         if (target == "") {
             webView.getEngine().loadContent("No word inserted!");
         } else {
@@ -83,6 +96,8 @@ public class HelloController implements Initializable {
     @FXML
     public void searchOnClick(MouseEvent event) {
         target = listView.getSelectionModel().getSelectedItem();
+        speak.setOpacity(100);
+        webView.setOpacity(100);
         if (target == null || target.isEmpty()) {
             webView.getEngine().loadContent("Nothing Selected!");
         } else {
@@ -111,6 +126,15 @@ public class HelloController implements Initializable {
         String s = text1.getText();
         text1.setText(text2.getText());
         text2.setText(s);
+        if (text1.getText().length() == 10) { // Tieng Viet
+            language.setText("EN-VI");
+            tooltip1.setText("Dịch tiếng Anh.");
+            textArea1.setPromptText("Gõ vào đây.");
+        } else {
+            language.setText("VI-EN");
+            tooltip1.setText("Translate Vietnamese.");
+            textArea1.setPromptText("Type here.");
+        }
     }
 
     @FXML
@@ -119,7 +143,10 @@ public class HelloController implements Initializable {
         listword = Dictionary.listWordTarget();
         listView.getItems().clear();
         listView.getItems().addAll(listword);
+        listView.setOpacity(0);
+        speak.setOpacity(0);
         searchBox.setText("");
+        webView.setOpacity(0);
         webView.getEngine().loadContent("");
         updated = true;
     }
